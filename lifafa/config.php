dee9eb1e8618ea7b6785f686e113c4a95699e223
Afx-Abu <aswinjr84@gmail.com>
2024-08-07 09:04:59 +0530

    first commit

---
+++ lifafa/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'fastwinn_lucky111';
+$DATABASE_PASS = 'fastwinn_lucky111';
+$DATABASE_NAME = 'fastwinn_lucky111';
+
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$mysql = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+?>
